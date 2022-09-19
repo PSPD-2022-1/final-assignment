a0b2b22fb5bb591a730d6007df1fd0eb5aa6ca56
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-09-18 21:47:47 -0300

    feat: add streaming tweet search

--- kafka-consumer/src/main/java/pspd/twitter/RecentTweetSearcher.java
+++ kafka-consumer/src/main/java/pspd/twitter/RecentTweetSearcher.java
@@ -1,12 +1,26 @@
 package pspd.twitter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import com.twitter.clientlib.ApiException;
 import com.twitter.clientlib.TwitterCredentialsBearer;
 import com.twitter.clientlib.api.TwitterApi;
+import com.twitter.clientlib.model.AddOrDeleteRulesRequest;
+import com.twitter.clientlib.model.AddOrDeleteRulesResponse;
+import com.twitter.clientlib.model.AddRulesRequest;
+import com.twitter.clientlib.model.DeleteRulesRequest;
+import com.twitter.clientlib.model.DeleteRulesRequestDelete;
+import com.twitter.clientlib.model.FilteredStreamingTweetResponse;
 import com.twitter.clientlib.model.Get2TweetsSearchRecentResponse;
+import com.twitter.clientlib.model.Rule;
+import com.twitter.clientlib.model.RuleNoId;
+import com.twitter.clientlib.model.RulesLookupResponse;
 import com.twitter.clientlib.model.Tweet;
 
 public class RecentTweetSearcher {
@@ -38,12 +52,121 @@ public class RecentTweetSearcher {
 
 	static void printPosts(List<Post> posts) {
 		for (Post p : posts) {
-			System.out.println("id=" + p.getId() + " " + "text='" + p.getText() + "'");
+			printPost(p);
 		}
 	}
 
-	public static void main(String[] args) throws ApiException {
-		List<Post> posts = RecentTweetSearcher.getPosts("#Brasil");
-		RecentTweetSearcher.printPosts(posts);
+	static void printPost(Post p) {
+		System.out.println("id=" + p.getId() + " " + "text='" + p.getText() + "'");
+	}
+
+	@javax.annotation.Nullable
+	static Stream<FilteredStreamingTweetResponse> getTweetStream(List<RuleNoId> rules) {
+		String twitterBearerToken = System.getenv("TWITTER_BEARER_TOKEN");
+		TwitterCredentialsBearer twitterCredentialsBearer = new TwitterCredentialsBearer(twitterBearerToken);
+		TwitterApi apiInstance = new TwitterApi(twitterCredentialsBearer);
+
+		RulesLookupResponse rulesLookup;
+		try {
+			rulesLookup = apiInstance.tweets().getRules().execute();
+
+			List<Rule> currentRules = rulesLookup.getData();
+
+			List<String> rulesIds = new ArrayList<String>();
+
+			AddOrDeleteRulesRequest addOrDeleteRulesRequest = new AddOrDeleteRulesRequest();
+
+			if (currentRules != null) {
+				System.out.println("Deleting current rules");
+
+				DeleteRulesRequest deleteRulesRequest = new DeleteRulesRequest();
+				DeleteRulesRequestDelete rulesToDelete = new DeleteRulesRequestDelete();
+
+				currentRules.forEach((rule) -> {
+					rulesIds.add(rule.getId());
+				});
+
+				rulesToDelete.setIds(rulesIds);
+
+				deleteRulesRequest.delete(rulesToDelete);
+
+				addOrDeleteRulesRequest.setActualInstance(deleteRulesRequest);
+
+				AddOrDeleteRulesResponse deleteResponse = apiInstance.tweets().addOrDeleteRules(addOrDeleteRulesRequest)
+						.dryRun(true).execute();
+
+				System.out.println(deleteResponse);
+
+				deleteResponse = apiInstance.tweets().addOrDeleteRules(addOrDeleteRulesRequest).execute();
+
+			} else {
+				System.out.println("No rules previously defined");
+			}
+
+			AddRulesRequest addRulesRequest = new AddRulesRequest();
+
+			addRulesRequest.add(rules);
+
+			addOrDeleteRulesRequest.setActualInstance(addRulesRequest);
+
+			AddOrDeleteRulesResponse addResponse = apiInstance.tweets().addOrDeleteRules(addOrDeleteRulesRequest)
+					.dryRun(true).execute();
+
+			System.out.println(addResponse);
+
+			addResponse = apiInstance.tweets().addOrDeleteRules(addOrDeleteRulesRequest).execute();
+
+			InputStream tweetStream = apiInstance.tweets().searchStream().execute(5);
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(tweetStream));
+
+			return reader.lines().map(json -> {
+				try {
+					return FilteredStreamingTweetResponse.fromJson(json);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}).filter(tweet -> {
+				return tweet != null;
+			});
+		} catch (ApiException e) {
+			System.err.println("Status code: " + e.getCode());
+			System.err.println("Reason: " + e.getResponseBody());
+			System.err.println("Response headers: " + e.getResponseHeaders());
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	static Post tweetToPost(FilteredStreamingTweetResponse tweet) {
+		return new Post(tweet.getData().getId(), tweet.getData().getText());
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.err.println("RecentTweetSearcher.getPosts(\"#Brasil\")");
+		List<Post> recentPosts = RecentTweetSearcher.getPosts("#Brasil");
+		RecentTweetSearcher.printPosts(recentPosts);
+
+		List<RuleNoId> rules = new ArrayList<RuleNoId>();
+
+		rules.add(new RuleNoId().tag("matchesHashTagBrasil").value("#Brasil"));
+		rules.add(new RuleNoId().tag("matchesHashTagEleições2022").value("#Eleições2022"));
+
+		System.err.println("RecentTweetSearcher.getTweetStream()");
+		Stream<FilteredStreamingTweetResponse> tweets = RecentTweetSearcher.getTweetStream(rules);
+
+		if (tweets != null) {
+			for (FilteredStreamingTweetResponse tweet : tweets.limit(5).toList()) {
+				System.out.println(tweet);
+				System.err.println("RecentTweetSearcher.tweetToPost()");
+				RecentTweetSearcher.printPost(tweetToPost(tweet));
+			}
+
+			tweets.close();
+		} else {
+			System.err.println("Empty tweet stream");
+		}
 	}
 }
